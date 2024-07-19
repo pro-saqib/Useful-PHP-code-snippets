ec4d919f8a8abaf0ccb244edc361ecacf0e5f2c3
Saqib <prosaqib2022@gmail.com>
2024-07-20 00:01:39 +0500

    Generate a Random Password

---
+++ Generate a Random Password.php
@@ -0,0 +1,6 @@
+function generatePassword($length = 12) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    return substr(str_shuffle($chars), 0, $length);
+}
+
+$password = generatePassword(16);
