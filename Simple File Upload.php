909db2aebfa1f845b4e4c32865d2a17424a259de
Saqib <prosaqib2022@gmail.com>
2024-07-20 00:05:19 +0500

    Get User's IP Address

---
+++ Simple File Upload.php
@@ -0,0 +1,10 @@
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo "The file ". basename($_FILES["file"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
